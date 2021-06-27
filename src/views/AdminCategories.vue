45cd845eb8d29a57dc74ae9d20bd4853ad7cd227
teddy <a21506qoo@gmail.com>
2021-06-27 20:32:47 +0800

    feat: add admin categories page with edit/cancel function.

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -47,32 +47,58 @@
       </thead>
       <tbody>
         <tr
-          v-for="category in categories"
-          :key="category.id"
-        >
-          <th scope="row">
-            {{ category.id }}
-          </th>
-          <td class="position-relative">
-            <div class="category-name">
-              {{ category.name }}
-            </div>
-          </td>
-          <td class="d-flex justify-content-between">
-            <button
-              type="button"
-              class="btn btn-link mr-2"
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              class="btn btn-link mr-2"
-              @click.prevent.stop="deleteCategory(category.id)"
-            >
-              Delete
-            </button>
-          </td>
+      v-for="category in categories"
+      :key="category.id"
+    >
+     <th scope="row">
+      {{ category.id }}
+     </th>
+        <td class="position-relative">
+          <div
+            v-show="!category.isEditing"
+            class="category-name"
+          >
+            {{ category.name }}
+          </div>
+          <input
+            v-show="category.isEditing"
+            v-model="category.name"
+            type="text"
+            class="form-control"
+          >
+          <span
+            v-show="category.isEditing"
+            class="cancel"
+            @click.prevent.stop="handleCancel(category.id)"
+          >
+            ✕
+          </span>
+        </td>
+        <td class="d-flex justify-content-between">
+          <button
+            v-show="!category.isEditing"
+            type="button"
+            class="btn btn-link mr-2"
+            @click.prevent.stop="toggleIsEditing(category.id)"
+          >
+            Edit
+          </button>
+          <button
+            v-show="category.isEditing"
+            type="button"
+            class="btn btn-link mr-2"
+            @click.prevent.stop="updateCategory({ categoryId: category.id, name: category.name})"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            class="btn btn-link mr-2"
+            @click.stop.prevent="deleteCategory(category.id)"
+          >
+            Delete
+          </button>
+        </td>
         </tr>
       </tbody>
     </table>
@@ -131,7 +157,12 @@ export default {
   methods: {
     // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
     fetchCategories () {
-      this.categories = dummyData.categories
+      this.categories = dummyData.categories.map(category => ({
+          ...category,
+          isEditing: false,
+          nameCached: ''
+        })
+      )
     },
     createCategory() {
       //TODO: 透過Api向後端伺服器新增餐廳類別
@@ -146,7 +177,66 @@ export default {
     deleteCategory(categoryId) {
       //TODO: 向後端伺服器刪除類別
       this.categories = this.categories.filter(category => category.id !== categoryId)
+    },
+    toggleIsEditing(categoryId) {
+      this.categories = this.categories.map(category => {
+        if(category.id === categoryId) {
+          return {
+            ...category,
+            isEditing: !category.isEditing,
+            nameCached: category.name
+          }
+        }
+        return category
+      })
+    },
+    updateCategory({ categoryId, name }) {
+      //TODO:向伺服器更新類別名稱
+      this.toggleIsEditing(categoryId)
+      console.log(name)
+    },
+    handleCancel(categoryId) {
+      this.categories = this.categories.map(category => {
+        if(category.id === categoryId) {
+          return {
+            ...category,
+            name: category.nameCached
+          }
+        }
+        return category
+      })
+      this.toggleIsEditing(categoryId)
     }
   }
 }
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
